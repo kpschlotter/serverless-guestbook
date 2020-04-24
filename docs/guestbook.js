1449b379a834e2f91c8a21b3da7a51856d57b5c0
Klaus-Peter Schlotter <kps@de.ibm.com>
2020-04-24 08:49:24 +0200

    url changed

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://2eec4d00.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
